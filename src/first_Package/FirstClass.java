7a8b484fa916fd57422b152eacf8c5f329ad549d
Kudret A <kudretnew@gmail.com>
2023-01-07 20:37:42 +0100

    upload new classes

--- src/first_Package/FirstClass.java
+++ src/first_Package/FirstClass.java
@@ -1,4 +1,7 @@
 package first_Package;
 
 public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("First Class");
+    }
 }
--- src/first_Package/SecondClass.java
+++ src/first_Package/SecondClass.java
@@ -1,4 +1,7 @@
 package first_Package;
 
 public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Second Class");
+    }
 }
